0e75adda51cdcbcc5cc82331b3ff7f18ddedebbe
justapig9020 <justapig9020@gmail.com>
2024-01-17 12:49:08 +0800

    NVIM: add markdown preview

--- nvim/lua/jup/plugins-setup.lua
+++ nvim/lua/jup/plugins-setup.lua
@@ -128,6 +128,23 @@ return packer.startup(function(use)
 	-- transparent background
 	use("xiyaowong/transparent.nvim")
 
+	-- Markwodn
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && npm install",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
+
 	if packer_bootstrap then
 		require("packer").sync()
 	end
